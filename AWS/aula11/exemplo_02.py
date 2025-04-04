1fcba36b9596c9502c6b45971d656f56a45f3c9a
Thiago Ferraz <thiagovdferraz@gmail.com>
2025-04-03 21:36:39 -0300

    incluir aula sobre lambda aws; subir codigo python para executar cron e como subir instalando uma library como requests

---
+++ AWS/aula11/exemplo_02.py
@@ -0,0 +1,48 @@
+import urllib3
+import json
+import os
+
+# Carregar variáveis de ambiente
+url = os.getenv('API_URL') # URL da API obtida das variáveis de ambiente
+api_key = os.getenv('CMC_API_KEY') # Chave da API obtida das variáveis de ambiente
+
+# Parâmetros da requisição para obter a cotação do Bitcoin
+parameters = {
+ 'symbol': 'BTC',  # Identificando o Bitcoin pelo símbolo
+ 'convert': 'USD'  # Convertendo a cotação para USD
+}
+
+# Headers com a chave da API
+headers = {
+ 'Accept': 'application/json',
+ 'X-CMC_PRO_API_KEY': api_key  # Obtendo a chave da API das variáveis de ambiente
+}
+
+# Criar um PoolManager para gerenciar conexões
+http = urllib3.PoolManager()
+
+# Função Lambda
+def lambda_handler(event, context):
+ try:
+     # Converte os parâmetros para o formato de query string
+     query_string = '&'.join([f'{key}={value}' for key, value in parameters.items()])
+     full_url = f"{url}?{query_string}"
+     
+     # Fazendo o request GET para a API
+     response = http.request('GET', full_url, headers=headers)
+     data = json.loads(response.data.decode('utf-8'))
+     
+     # Verificar se os dados do Bitcoin estão presentes na resposta
+     if 'data' in data and 'BTC' in data['data']:
+         bitcoin_data = data['data']['BTC']
+         usd_quote = bitcoin_data['quote']['USD']
+         
+         # Log da resposta
+         print(f"Cotação do Bitcoin obtida: {usd_quote}")
+         return f"Cotação do Bitcoin obtida: {usd_quote}"
+     else:
+         print("Erro ao obter a cotação do Bitcoin:", data.get('status', {}).get('error_message', 'Erro desconhecido'))
+         return "Erro ao obter a cotação do Bitcoin"
+
+ except urllib3.exceptions.HTTPError as e:
+     print(f"Erro na requisição: {e}")
